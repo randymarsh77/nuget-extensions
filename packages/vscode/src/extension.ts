adba75b831ba432cbb452f3e122e96421604a457
Matthew Preucil <mattpsbs@gmail.com>
2019-03-07 20:36:43 -0800

    Fix Lerna configuration.

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,14 +1,19 @@
 import * as vscode from 'vscode';
-import { link } from '../../../lib/src';
+import { link } from 'nuget-extensions-lib';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('extension.nugex.link', () => {
-			const c = vscode.workspace.getConfiguration('NuGet Extensions');
-			const p = c.get('defaultProjects');
-			vscode.window.showInformationMessage(JSON.stringify(p));
-			// link();
-		}),
+			const config = vscode.workspace.getConfiguration('Nuget Extensions');
+			const projects = (config.get<string>('defaultProjects') || '').split(',');
+			if (projects.length !== 0) {
+				link(projects);
+			} else {
+				vscode.window.showInformationMessage(
+					'You need to set the Nuget Extensions Default Projects setting.'
+				);
+			}
+		})
 	);
 }
 
