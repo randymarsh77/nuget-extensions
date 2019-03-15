a87180da7bcd56bb38f787d234255821aabc9d4d
Matthew Preucil <mattpsbs@gmail.com>
2019-03-14 19:42:45 -0700

    Fix bootstrapper.

--- packages/vscode/tests/bootstrap.js
+++ packages/vscode/tests/bootstrap.js
@@ -1,13 +1,14 @@
 #!/usr/local/bin/node
-import { execFileSync } from 'child_process';
-import * as fs from 'fs';
+const { execFileSync } = require('child_process');
+const path = require('path');
+const process = require('process');
 
 console.log('Welcome to the NuGet Extensions test environment bootstrapper!');
 console.log(
 	'This script will clone some example open source repositories that consume and publish '
 );
 
-const projectsPath = path.join(pwd(), 'projects');
+const projectsPath = path.join(process.cwd(), 'projects');
 const manifest = ['https://github.com/Faithlife/FaithlifeAnalyzers.git'];
 
 manifest.reduce((acc, v) => {
