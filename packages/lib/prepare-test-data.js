3529a236c3c4453c9bee3449765f82e257e2f2fb
randymarsh77 <mattbsbs@gmail.com>
2019-07-18 23:53:50 -0700

    Specify correct output directory.

--- packages/lib/prepare-test-data.js
+++ packages/lib/prepare-test-data.js
@@ -2,12 +2,13 @@ const path = require('path');
 const shell = require('shelljs');
 
 const solutionDirectory = path.join(__dirname, 'tests', 'data', 'TestData');
+const outputDirectory = path.join(__dirname, 'tests', 'data', 'packages');
 
 shell.exec(`pwd`, {
 	stdio: [process.stdin, process.stdout, process.stderr],
 });
 
-shell.exec(`ls -la`, {
+shell.exec(`ls -la ${solutionDirectory}`, {
 	stdio: [process.stdin, process.stdout, process.stderr],
 });
 
@@ -15,7 +16,7 @@ shell.exec(
 	`dotnet pack ${path.join(
 		solutionDirectory,
 		'TestData.sln'
-	)} -c Debug --version-suffix Debug --output ${path.join(__dirname, '..', '..', 'packages')}`,
+	)} -c Debug --version-suffix Debug --output ${outputDirectory}`,
 	{
 		stdio: [process.stdin, process.stdout, process.stderr],
 	}
