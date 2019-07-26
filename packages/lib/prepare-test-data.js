5a25bcddd9e3ede7ac7df6025acfd4fb6faa9f1f
randymarsh77 <mattbsbs@gmail.com>
2019-07-26 08:18:04 -0700

    Remove build debug logging.

--- packages/lib/prepare-test-data.js
+++ packages/lib/prepare-test-data.js
@@ -4,14 +4,6 @@ const shell = require('shelljs');
 const solutionDirectory = path.join(__dirname, 'tests', 'data', 'TestData');
 const outputDirectory = path.join(__dirname, 'tests', 'data', 'packages');
 
-shell.exec(`pwd`, {
-	stdio: [process.stdin, process.stdout, process.stderr],
-});
-
-shell.exec(`ls -la ${solutionDirectory}`, {
-	stdio: [process.stdin, process.stdout, process.stderr],
-});
-
 shell.exec(
 	`dotnet pack ${path.join(
 		solutionDirectory,
