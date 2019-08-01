f73d55ee7e17217ff01b7454fe1d55ab4265f8bc
randymarsh77 <mattbsbs@gmail.com>
2019-07-31 21:14:08 -0700

    Add TestConsoleApp dev environment.

---
+++ packages/vscode/tests/projects/TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Faithlife.Utility;
+
+namespace TestConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (Scope.Create(() => Console.WriteLine("World!")))
+				Console.Write("Hello ");
+			Console.Read();
+		}
+	}
+}
