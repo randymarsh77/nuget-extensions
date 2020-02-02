d6651f5a2838ae2be775b63a49e0a74a11cbc1b9
randymarsh77 <mattpsbs@gmail.com>
2020-02-01 20:48:56 -0800

    Remove circular stream stringify in Windows build.

--- packages/lib/embed-binaries.js
+++ packages/lib/embed-binaries.js
@@ -4,9 +4,13 @@ const shell = require('shelljs');
 const projectDirectory = path.join('..', '..', 'tools', 'NuGetPackageInfo', 'NuGetPackageInfo');
 
 shell.config.execPath = shell.which('node').toString();
-shell.exec(`dotnet build ${path.join(projectDirectory, 'NuGetPackageInfo.csproj')} -c Release`, {
-	stdio: [process.stdin, process.stdout, process.stderr],
-});
+const { code } = shell.exec(
+	`dotnet build ${path.join(projectDirectory, 'NuGetPackageInfo.csproj')} -c Release`
+);
+if (code !== 0) {
+	console.error('Failing build due to last error.');
+	process.exit(1);
+}
 
 const from = path.join(projectDirectory, 'bin', 'Release', 'netcoreapp3.1');
 const to = path.join('out', 'bin', 'NuGetPackageInfo');
