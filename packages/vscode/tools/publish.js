fe04c2d4f1c4d80bc65bbe31380a7a1e123f1de3
randymarsh77 <mattpsbs@gmail.com>
2020-06-04 01:18:49 -0700

    fix(vscode): fix path in publish script.

--- packages/vscode/tools/publish.js
+++ packages/vscode/tools/publish.js
@@ -1,12 +1,10 @@
 const path = require('path');
 const shell = require('shelljs');
 
-const pathToPackage = pkg => path.join(__dirname, '..', '..', 'packages', pkg);
-
 module.exports = {
 	publish: () => {
 		const vscode = {
-			cwd: pathToPackage('vscode'),
+			cwd: path.join(__dirname, '..'),
 		};
 		shell.exec('npx vsce package --yarn', vscode);
 		shell.exec(`npx vsce publish -p ${process.env.VSCE_TOKEN} --yarn`, vscode);
